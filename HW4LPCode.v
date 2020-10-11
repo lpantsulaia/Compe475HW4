10ac6e40232c20073782d02542669b6fef644c0d
unknown <lpantsulaia1015@sdsu.edu>
2020-10-11 23:26:17 +0400

    coments

--- HW4LPCode.v
+++ HW4LPCode.v
@@ -1,32 +1,12 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    22:17:31 10/11/2020 
-// Design Name: 
-// Module Name:    HW4LPCode 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module HW4LPCode(
-		//input clock,
 		input wire[31:0] instruction,
 		output reg[2:0] instr_type,
 		output reg[3:0] data_instr_type
     );
 
 	 always @(*) begin
-	 if(instruction[27] && !instruction[26]) begin
+	 if(instruction[27] && !instruction[26]) begin // initial logic
 		 instr_type=2'b11;
 	 end
 	 
@@ -42,7 +22,8 @@ module HW4LPCode(
 		 instr_type=2'b00;
 	 end
 	
-	 if(instruction[25]) begin
+	 if(instruction[25]) begin //second logic
+
 		 data_instr_type = 3'b001;
 	 end
 	 
